352ebc30e5645b3db33f2d091b4b27d145748c8f
Malo Skrylevo <majioa@yandex.ru>
2018-01-18 00:06:38 +0300

    Initial commit

---
+++ lib/rails/api/scheme.rb
@@ -0,0 +1,9 @@
+require "rails/api/scheme/version"
+
+module Rails
+  module Api
+    module Scheme
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/api/scheme/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Api
+    module Scheme
+      VERSION = "0.1.0"
+    end
+  end
+end
